5d29ba3e072b66df370e4df5b2006233498665ce
TristeTempsChang <tristan.tran2111@gmail.com>
2024-09-19 00:30:43 +0200

    most responsive finish

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,12 @@ module.exports = {
       'Montserrat': ['"Montserrat"'],
       'OpenSans' : ['"Open Sans"']
     },
+    screens: {
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '1/2xl': '1400px'
+    },
   },
   plugins: [],
 }
