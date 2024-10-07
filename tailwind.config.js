b7eea802e960a5fb01cfb5fd5446b5dbee39d7d2
Tristan-wlc <tristan@welovecustomers.fr>
2024-10-07 12:16:02 +0200

    change

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,8 @@ module.exports = {
       'md': '768px',
       'lg': '1024px',
       'xl': '1280px',
-      '1/2xl': '1400px'
+      '1/2xl': '1400px',
+      '2xl': '1500px'
     },
     container: {
       padding: {
